f15a5a324be5473bc79f79b199d07e356d8f2d4e
Sophie 'Fynne' Schumann <me@fynne.eu>
2021-01-05 08:56:56 +0100

    Better code grouping inspired by gf 💜💜💜💜💜💜💜💜💜💜💜

--- decoder.py
+++ decoder.py
@@ -1,12 +1,74 @@
+import re
+
+RE_PLURAL_SEG = re.compile(r'(\<.+?\>)')
+
 def decode_pluralcode(code: str):
     c = {}
+
+    # scan for headmate segments
+    if not '<' in code:
+        raise Exception('no headmates in pluralcode found or malformed code')
+
+    m = RE_PLURAL_SEG.findall(code)
+    for segment in m:
+        headmate = {}
+        if ':' in segment:
+            headmate['name'] = segment[1:segment.index(':')]
+            segment = segment[segment.index(':'):]
+
+        for part in segment.split(' '):
+            if part[0] == 'G':               
+                gd = {
+                    'm': 'male',
+                    'f': 'female',
+                    'nb': 'non-binary',
+                    'o': 'other'
+                }
+                headmate['gender'] = gd[part[1:]]
+
+            elif part[0] == 'S':                
+                gd = {
+                    'h': 'human',
+                    's': 'spiritual',
+                    '~': 'shape-shifter',
+                    'm': 'mammal',
+                    'mc': 'canine',
+                    'mf': 'feline'
+                }
+                form = ""
+                if part[1:] in gd:
+                    form += gd[part[1:]]
+                else:
+                    form += part[1:]
+                headmate['form'] = form
+
+            elif part[0] == 'A':                
+                if part[1:] == 'x':
+                    headmate['age'] = 'unknown'
+                else:
+                    headmate['age'] =  int(part[1:])
+
+            elif part[0] == 'O':
+                gd = {
+                    'b': 'born with body',
+                    't': 'traumagenic',
+                    'n': 'natural',
+                    'i': 'intentional',
+                    'u': 'unknown'
+                }
+                headmate['origin'] = gd[part[1:2]]
+        
+        if 'headmates' in c:
+            c['headmates'].append(headmate)
+        else:
+            c['headmates'] = [headmate]
+    
 
     for part in code.split(' '):
         if part[0] == 'P':
             if part[1] != 'X':
                 raise Exception('pluralcode version not implemented')
             c['version'] = part[2:]
-
         elif part[0] == 'N':
             c['number'] = int(part[1:])
         elif part[0] == 'D':
@@ -18,73 +80,15 @@ def decode_pluralcode(code: str):
         elif part[0] == 'W':
             c['openness'] = int(part[1:])
         
-        elif part[0] == 'G':
-            genders = []
-            if 'genders' in c:
-                genders = c['genders']
-            
-            gd = {
-                'm': 'male',
-                'f': 'female',
-                'nb': 'non-binary',
-                'o': 'other'
-            }
-            genders.append(gd[part[1:]])
-            c['genders'] = genders
-
-        elif part[0] == 'S':
-            form = []
-            if 'form' in c:
-                form = c['form']
-            
-            gd = {
-                'h': 'human',
-                's': 'spiritual',
-                '~': 'shape-shifter',
-                'm': 'mammal',
-                'mc': 'canine',
-                'mf': 'feline'
-            }
-            if part[1:] in gd:
-                form.append(gd[part[1:]])
-            else:
-                form.append(part[1:])
-            c['form'] = form
-
-        elif part[0] == 'A':
-            age = []
-            if 'age' in c:
-                age = c['age']
-            
-            if part[1:] == 'x':
-                age.append('unknown')
-            else:
-                age.append(int(part[1:]))
-            c['age'] = age
-
-        elif part[0] == 'O':
-            origin = []
-            if 'origin' in c:
-                origin = c['origin']
-            
-            gd = {
-                'b': 'born with body',
-                't': 'traumagenic',
-                'n': 'natural',
-                'i': 'intentional',
-                'u': 'unknown'
-            }
-            origin.append(gd[part[1:]])
-            c['origin'] = origin
-
-        
 
     return c
 
 if __name__ == '__main__':
-    plural = decode_pluralcode("PX0.1 N2 D9 C5 Q8 W4 Gf Gnb Sh S~ A1 Ax Oi Ot")
+    plural = decode_pluralcode("PX0.1 N2 D9 C5 Q8 W4 <Lara: Gf A1 Oi> <Go S~ Ax Ot>")
     from json import dumps
     print(dumps(plural, indent=4))
+    import sys
+    sys.exit(0)
     try:
         from toml import dumps
         print(dumps(plural))
